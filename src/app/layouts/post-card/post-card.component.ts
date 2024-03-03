d3e46e0592eb9d24bc0f6373a9fd96e2fcf3a8a9
Muanester <stevenmutisya999@gmail.com>
2024-03-04 02:16:32 +0300

    load posts from db

--- src/app/layouts/post-card/post-card.component.ts
+++ src/app/layouts/post-card/post-card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
-  styleUrl: './post-card.component.css'
+  styleUrl: './post-card.component.css',
 })
-export class PostCardComponent {
+export class PostCardComponent implements OnInit {
+  @Input() postData: any;
+  postImgUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    if (this.postData && this.postData.data && this.postData.data.postImgPath) {
+      this.postImgUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        this.postData.data.postImgPath
+      );
+    }
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  featuredPostsArray: Array<any>;
 
+  constructor(private postService: PostsService) {
+    this.postService.loadFeatured().subscribe({
+      next: (data) => {
+        this.featuredPostsArray = data;
+      },
+    });
+  }
 }
